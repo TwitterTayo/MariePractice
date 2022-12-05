9d61a8827535d9ea26452d28d8a46cc62a57ac0f
Your Name <you@example.com>
2022-12-05 19:24:49 +0000

    tested and approved

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 This is a deployment script
 deploy python apps
 deployment5 master
+<<<<<<< HEAD
 This is really weird
+=======
+deploy to staging area
+echo good job
+>>>>>>> 696420b (tested and approved)
